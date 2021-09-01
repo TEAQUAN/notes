ae78dde54456f808224b680934ab5437cab762d7
TEAQUAN <88717213+TEAQUAN@users.noreply.github.com>
2021-09-01 20:42:44 +0100

    Update create.js

--- create.js
+++ create.js
@@ -20,5 +20,19 @@ saveButtonElement.addEventListener("click", (e) => {
     window.location.href = "index.html"
 })
 
+let input = document.querySelector(".forms");
+let button = document.querySelector(".success");
+
+button.disabled = true; //setting button state to disabled
+
+input.addEventListener("change", stateHandle);
+
+function stateHandle() {
+    if (document.querySelector(".forms").value === "") {
+        button.disabled = true; //button remains disabled
+    } else {
+        button.disabled = false; //button is enabled
+    }
+}
     
 	
